8edd0724ba727fc0a4242272e2b76bfa8c995a91
Tom Christiansen-Salameh <tomsalameh@gmail.com>
2019-01-14 21:03:21 -0800

    add comments and documentation in source code

--- bike_lights/bike_lights.ino
+++ bike_lights/bike_lights.ino
@@ -1,3 +1,16 @@
+/*
+This code will animate a "caterpillar" style of light display.
+Lights will start in a line of PIX_PER_RUN pixels, and "crawl" up
+towards the headtube, starting again at the bottom of the downtube.
+
+The rate at which the lights "crawl" changes depending on how fast the
+wheel is rotating. Maximum and minimum values can be configured.
+
+The color of the lights also changes depending on speed, and maximum and
+minimum colors can be selected. The lights will step through a smooth range
+of intermediate colors as speed increases until they reach the maximum.
+*/
+
 #include <StaticThreadController.h>
 #include <Thread.h>
 #include <ThreadController.h>
@@ -5,38 +18,39 @@
 
 #define PIXEL_PIN    6
 #define SENSOR_PIN   8
-#define STRIP_SIZE   68
-#define PIX_PER_RUN  3
+#define STRIP_SIZE   68    // number of pixels in light strip
+#define PIX_PER_RUN  3     // number of pixels in "caterpillar"
 
-#define COLOR_ROT_TIME_MIN  75
-#define COLOR_ROT_TIME_MAX  1275
+#define COLOR_ROT_TIME_MIN  75      // milliseconds between wheel rotations below which we're at the fastest color
+#define COLOR_ROT_TIME_MAX  1275    // milliseconds between wheel rotations below which we're at teh slowest color
 
-#define R_COLOR_MIN      255
-#define G_COLOR_MIN      0
-#define B_COLOR_MIN      0
-#define R_COLOR_MAX      0
-#define G_COLOR_MAX      255
-#define B_COLOR_MAX      100
-#define R_RANGE          (double) (R_COLOR_MAX - R_COLOR_MIN)
-#define G_RANGE          (double) (G_COLOR_MAX - G_COLOR_MIN)
-#define B_RANGE          (double) (B_COLOR_MAX - B_COLOR_MIN)
+#define R_COLOR_MIN      255    // red value at slowest speed
+#define G_COLOR_MIN      0      // green value at slowest speed
+#define B_COLOR_MIN      0      // blue value at slowest speed
+#define R_COLOR_MAX      0      // red value at fastest speed
+#define G_COLOR_MAX      255    // green value at fastest speed
+#define B_COLOR_MAX      100    // blue value at fastest speed
+#define R_RANGE          (double) (R_COLOR_MAX - R_COLOR_MIN)    // total range of red values
+#define G_RANGE          (double) (G_COLOR_MAX - G_COLOR_MIN)    // total range of green values
+#define B_RANGE          (double) (B_COLOR_MAX - B_COLOR_MIN)    // total range of blue values
 
-#define RATE_ROT_TIME_MIN   51
-#define RATE_ROT_TIME_MAX   4000
-#define RATE_MIN         10
-#define RATE_MAX         400
-#define RATE_RANGE       (double) (RATE_MAX - RATE_MIN)
+#define RATE_ROT_TIME_MIN   51     // milliseconds between wheel rotations below which we're at the fastest change rate
+#define RATE_ROT_TIME_MAX   4000   // milliseconds between wheel rotations above which we're at the slowest change rate
+#define RATE_MIN            10     // minimum number of milliseconds between transitions
+#define RATE_MAX            400    // maximum number of milliseconds between transitions
+#define RATE_RANGE          (double) (RATE_MAX - RATE_MIN)    // total range of transition rate values
 
-#define STOPPED          2000
-#define R_STOPPED        255
-#define G_STOPPED        0
-#define B_STOPPED        0
-#define STOPPED_RATE     400
+#define STOPPED          2000    // if wheel rotation takes more milliseconds than this, we consider the bike stopped
+#define R_STOPPED        255     // red value when we're stopped
+#define G_STOPPED        0       // green value when we're stopped
+#define B_STOPPED        0       // blue value when we're stopped
+#define STOPPED_RATE     400     // milliseconds between animation transition when we're stopped
 
 class PixelThread: public Thread {
+/* Class to encapsulate the thread controlling a light strip */
   public:
     Adafruit_NeoPixel strip;
-    uint8_t pix_per_run;
+    uint8_t pix_per_run;    // number of pixels in "caterpillar"
     uint16_t index;
     uint32_t color;
 
@@ -51,6 +65,13 @@ class PixelThread: public Thread {
     }
 
     uint32_t calculateColor(long rotation_time) {
+      /* This function calculates what color the pixels
+      should be based on the number of milliseconds one wheel
+      revolution takes. It finds where the rotation time falls
+      between our minimum and maximum rotation times, translates
+      that into a percentage, and then uses the percentage to
+      calculate how far between min and max each RGB component should
+      lie. */
       double pctg;
 
       if (rotation_time >= STOPPED)
@@ -71,8 +92,9 @@ class PixelThread: public Thread {
 };
 
 class SpeedThread: public Thread {
+/* Class to encapsulate a thread that measures the time of each wheel rotation */
   public:
-    uint16_t rate;
+    uint16_t rate;    // the update rate we will calculate for the animation
     long rotation_time;
 
     long _timeOfLastHit;
@@ -88,6 +110,8 @@ class SpeedThread: public Thread {
   }
 
   void updateRotation() {
+  /* updates the time since the last rotation,
+  should be called in a tight loop */
     if (_reset) {
       _reset = (digitalRead(SENSOR_PIN) == LOW);
       return;
@@ -100,6 +124,9 @@ class SpeedThread: public Thread {
   }
 
   uint16_t calculateRate() {
+  /* Based on the milliseconds it takes the wheel to rotate,
+  calculates the number of milliseconds between animation updates.
+  It maps the rotation time linearly against the min and max rates. */
     double pctg;
 
     if (rotation_time >= STOPPED)
@@ -115,6 +142,8 @@ class SpeedThread: public Thread {
   }
 
   void setRate() {
+  /* callback to periodically calculate rate, should be called
+  more frequently than the fastest rotation time */
     rotation_time = max(_timeSinceLastRotation, millis() - _timeOfLastHit);
     rate = calculateRate();
   }
@@ -140,6 +169,9 @@ void speedCallback() {
 }
 
 void lightStripCallback() {
+/* Callback to update the light strip.
+Clears the last pixel in the "caterpillar",
+and fills in the next pixel ahead. */
   uint16_t n = strip.numPixels();
   if (pt.index > 0) {
     strip.setPixelColor(pt.index-1, 0);
@@ -182,10 +214,11 @@ void setup() {
 
   controller.add(&pt);
   controller.add(&st);
-  Serial.begin(9600);
 }
 
 void loop() {
+  // Rather than explicitly calling as another pseudo-thread,
+  // found it more effective to recalculate rotation rate on every loop.
   st.updateRotation();
   controller.run();
 }
